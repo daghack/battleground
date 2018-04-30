c70cd4710c23d4cb5da00d3799fa8e9ccc66c6b9
Talon Bowler <nolat301@gmail.com>
2018-04-29 19:40:02 -0700

    Add sql code

---
+++ game/logic/sql.go
@@ -0,0 +1,83 @@
+package types // import "github.com/daghack/battlegrounds/game/logic"
+
+import (
+	"os"
+	_ "github.com/lib/pq"
+	"github.com/jmoiron/sqlx"
+	"io/ioutil"
+)
+
+const fetchPlayer string = `select * from players where id=$1`
+const createPlayer string = `insert into players (id, username, passkey) values (:id, :username, :passkey)`
+
+const fetchGame string = `select * from active_games where id=$1`
+const createGame string = `insert into active_games (board_size, piece_count, board_state) values (:board_size, :piece_count, :board_state)`
+const joinGame string = `insert into active_players (game_id, player_id) values ($1, $2)`
+
+type DBHandler struct {
+	db *sqlx.DB
+}
+
+func loadDatabase(postgresStr, loadfile string) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("postgres", postgresStr)
+	if err != nil {
+		return nil, err
+	}
+	fileH, err := os.Open(loadfile)
+	if err != nil {
+		return nil, err
+	}
+	defer fileH.Close()
+	schema, err := ioutil.ReadAll(fileH)
+	if err != nil {
+		return nil, err
+	}
+	_, err = db.Exec(string(schema))
+	return db, nil
+}
+
+func NewDBHandler(postgresStr, loadfile string) (*DBHandler, error) {
+	db, err := loadDatabase(postgresStr, loadfile)
+	if err != nil {
+		return nil, err
+	}
+	return &DBHandler{ db : db }, nil
+}
+
+func (dbh *DBHandler) FetchPlayer(playerId string) (*Player, error) {
+	player := &Player{}
+	err := dbh.db.Get(player, fetchPlayer, playerId)
+	if err != nil {
+		return nil, err
+	}
+	return player, nil
+}
+
+func (dbh *DBHandler) CreatePlayer(player *Player) error {
+	_, err := dbh.db.NamedExec(createPlayer, player)
+	return err
+}
+
+func (dbh *DBHandler) FetchGame(gameId string) (*ActiveGame, error) {
+	activeGame := &ActiveGame{}
+	err := dbh.db.Get(activeGame, fetchGame, gameId)
+	if err != nil {
+		return nil, err
+	}
+	return activeGame, nil
+}
+
+func (dbh *DBHandler) CreateGame(boardSize, pieceCount int) error {
+	game := &ActiveGame{
+		BoardSize : boardSize,
+		PieceCount : pieceCount,
+		BoardState : []byte(`{}`),
+	}
+	_, err := dbh.db.NamedExec(createGame, game)
+	return err
+}
+
+func (dbh *DBHandler) JoinGame(playerId, gameId string) error {
+	_, err := dbh.db.Exec(joinGame, playerId, gameId)
+	return err
+}
---
+++ sql/state0.sql
@@ -0,0 +1,36 @@
+create extension if not exists pgcrypto;
+
+create table if not exists players (
+	id uuid primary key default gen_random_uuid(),
+	username string not null,
+	pass_key string not null
+);
+
+create table if not exists unit_types (
+	id text primary key,
+	base_health int not null,
+	base_movement int not null,
+	base_cost int not null,
+	attack_pattern bytea
+);
+
+create table if not exists active_games (
+	id uuid primary key default gen_random_uuid(),
+	board_size int not null,
+	piece_count int not null,
+	board_state bytea not null
+);
+
+create table if not exists active_players (
+	game_id uuid not null references active_games(id) on delete cascade,
+	player_id uuid not null references players(id) on delete cascade
+);
+
+create table if not exists available_unit_types (
+	player_id uuid not null references players(id) on delete cascade,
+	unit_type_id null references unit_types(id) on delete cascade
+);
+
+insert into unit_types (
+	id, base_health, base_movement, base_cost, attack_pattern
+) values ( "footman", 1, 4, 1, null) on conflict do nothing;
